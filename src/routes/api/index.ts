7c48846385cfcf37490171f84f3ca164131fe508
Samuel Wang <chieh0919@gmail.com>
2023-11-15 23:55:05 +0800

    add api endpoint

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,5 +1,9 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const router = express.Router();
+
+router.get('/', (_req: Request, res: Response) => {
+  res.send('API Endpoint');
+});
 
 export default router;
