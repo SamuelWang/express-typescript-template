79c6c88bbd6cc4959d2be466f3b181b6d2fb0acc
Samuel Wang <chieh0919@gmail.com>
2023-11-17 20:40:10 +0800

    add health check API

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,9 +1,8 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import healthCheckRouter from './health-check';
 
 const router = express.Router();
 
-router.get('/', (_req: Request, res: Response) => {
-  res.send('API Endpoint');
-});
+router.use('/health-check', healthCheckRouter);
 
 export default router;
